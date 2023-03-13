b6a437413f7f0f2da998c1c3a7ca7a272dd17dbc
aramfilak <aram.filak@outlook.de>
2023-03-13 23:55:12 +0100

    update Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import "./App.sass";
 import SearchBox from "./components/SearchBox";
-import Result from "./components/Result";
-import Toolbar from "./components/Toolbar";
+import moon from "./assets/images/icon-moon.svg";
+import sun from "./assets/images/icon-sun.svg";
+import { useState } from "react";
 
 const App = () => {
+  const [theme, setTheme] = useState("");
+
+  const switchTheme = () => {
+    if (!theme) setTheme("dark");
+    else setTheme("");
+  };
   return (
-    <main className="App">
+    <main className={`App ${theme}`}>
       <div className="container">
-        <Toolbar />
+        <header>
+          <h2>devfinder</h2>
+          <button className="switch-theme-btn" onClick={switchTheme}>
+            <p>{theme ? "light" : "dark"}</p>
+            <img src={theme ? sun : moon} alt="moon-icon" />
+          </button>
+        </header>
         <SearchBox />
       </div>
     </main>
